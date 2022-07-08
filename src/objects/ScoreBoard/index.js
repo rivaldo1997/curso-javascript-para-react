126d01732f474054514099f297220a7ab5a27b06
Rivaldo Silva <rivaldodowneyy@gmail.com>
2022-07-08 11:27:38 -0300

    Add ScoreBoard CSS

--- src/objects/ScoreBoard/index.js
+++ src/objects/ScoreBoard/index.js
@@ -1,8 +1,9 @@
 import PlayerName from "../../components/PlayerName";
+import "./style.css";
 
 function ScoreBoard() {
     return `
-    <header class="Score-Board">
+    <header class="score-board">
         ${PlayerName("Player1")}
         ${PlayerName("Player2")}
     </header>
